17dd67cc55ef7e27b8247e6f2f9b89eb29b9b60b
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-08 08:59:02 +0500

    Connected api.js and packages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './index.scss';
+import { BrowserRouter } from 'react-router-dom';
+const App = lazy(() => import('./App'))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
-);
+    <BrowserRouter>
+        <Suspense fallback={<p>Loading...</p>}>
+            <App />
+        </Suspense>
+    </BrowserRouter>
+)
