23704e97f72507ea373bb6bf586fd3ed062fd317
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-14 08:04:09 +0500

    I connected Skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './scss/App.scss';
 import Nav from './layout/nav/Nav';
 import AllRoutes from './routes/AllRoutes'
+import { SkeletonTheme } from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 function App() {
   return (
-    <div className="App">
+    <>
+      <SkeletonTheme baseColor='#b2b2b2' highlightColor='lightgray' duration={0.5}>
       <Nav />
       <AllRoutes />
-    </div>
+      </SkeletonTheme>
+    </>
   );
 }
 
--- src/components/reel/Reel.jsx
+++ src/components/reel/Reel.jsx
@@ -5,23 +5,24 @@ import { Container } from '../../utils/index'
 import ArticleCard from "../article-card/ArticleCard"
 import Skeleton from 'react-loading-skeleton'
 
+
 const Reel = () => {
     const [data, setData] = useState([])
-    useEffect(()=>{
-            apiInstanse("/api/posts")
+    useEffect(() => {
+        apiInstanse("/api/posts")
             .then(response => {
                 setData(response.data.data)
                 console.log(response.data.data);
             })
             .catch(error => console.log(error))
     }, [])
-  return (
-    <div className='home__reel'>
-        <Container>
+    return (
+        <div className='home__reel'>
+            <Container>
                 <h2>Today's trending</h2>
-            <div className="reel__wrapper">
-                {
-                    data.map((article, index) => 
+                <div className="reel__wrapper">
+                    {
+                        data.map((article, index) =>
                             <ArticleCard
                                 image={article.image}
                                 title={article.title}
@@ -29,15 +30,15 @@ const Reel = () => {
                                 author={article.author}
                                 createAt={article.createAt}
                                 category={article.category}
-                            />
-                        )
-                }
-                
-            </div>
-            <Skeleton/>
-        </Container>
-    </div>
-  )
+                            /> 
+                            )
+                        }
+                        
+
+                </div>
+            </Container>
+        </div>
+    )
 }
 
 export default Reel
